2e5af50ac59aa75880bc0e3b36f4d39d0c24a350
superp00t <superp00t@tutanota.com>
2024-06-28 01:05:45 -0400

    feat(build): add zig build script

---
+++ build.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+const system = @import("system");
+
+pub fn build(b: *std.Build) void {
+  const target = b.standardTargetOptions(.{});
+  const optimize = b.standardOptimizeOption(.{});
+
+  // mem library
+  const lib = b.addStaticLibrary(.{
+    .name = "mem",
+    .target = target,
+    .optimize = optimize
+  });
+
+  system.add_defines(lib);
+
+  lib.linkLibCpp();
+
+  lib.addIncludePath(b.path("."));
+
+  lib.addCSourceFiles(.{
+    .files = &.{
+      "mem/Memory.cpp"
+    },
+    .flags = &.{"-std=c++11"}
+  });
+
+  b.installArtifact(lib);
+}
